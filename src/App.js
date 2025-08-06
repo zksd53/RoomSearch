0a5dba7b1892e2b163874a3069c4e4624d507c99
Zaki Saud <zakisaud2023@gmail.com>
2025-08-06 17:06:13 -0500

    Starting of the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to RoomSearch</h1>
+      <p>Find affordable rooms and roommates near you.</p>
+      <button className="button">Get Started</button>
+    </div>
+  );
+}
+
+
+export default App;
