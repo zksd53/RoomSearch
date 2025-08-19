b292b849ec781f610aa7813c58ac34604ed63a4e
Zaki Saud <zakisaud2023@gmail.com>
2025-08-18 20:33:40 -0500

    Update heading bar layout, adjust auth buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import './styles.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome to RoomSearch</h1>
-      <p>Find affordable rooms and roommates near you.</p>
-      <button className="button">Get Started</button>
+    <div className="heading-bar">
+      {/* Logo + Home Icon */}
+      <div className="home-logo-container">
+        <div className="homeBox">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="40" height="40">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+        </div>
+        <h1>RoomSearch</h1>
+      </div>
+
+      {/* Navigation Buttons - centered */}
+      <div className="nav-buttons">
+        <button>Browse</button>
+        <button>Experiences</button>
+        <button>Host your accommodation</button>
+        <button>Help</button>
+      </div>
+
+      {/* Login + Signup Buttons - slightly inside from the right */}
+      <div className="auth-buttons">
+        <button className="icon-button">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon" height="20px" width="20px">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+          </svg>
+          Login
+        </button>
+
+        <button className="sign-up">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon" height="20px" width="20px">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+          Signup
+        </button>
+      </div>
     </div>
   );
 }
-
 
 export default App;
